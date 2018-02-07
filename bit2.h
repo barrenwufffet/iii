50257f7bc6a78028967dca52d06750ead3a1adbe
Gavin Smith <Gavin.Smith@tufts.edu>
2018-02-06 22:16:51 -0500

    Changes to bit2 and design

--- bit2.h
+++ bit2.h
@@ -7,19 +7,19 @@
 #ifdef BIT2_H_
 #define BIT2_H_
 
-#define B bit2_B
-typedef struct B *B;
+#define B bit2_T
+typedef struct T *T;
  
 T * bit2_new(int height, int width, int dataSize);
-void * bit2_at(B bit2, int row, int col);
-int bit2_width(B bit2); 
-int bit2_height(B bit2);
-int bit2_height(B bit2);
-void bit2_free(B * bit2);
-void bit2_map_row_major(B bit2, void apply(void *bit,
-        void *cl), void *cl);
-void bit2_map_col_major(B bit2, void apply(void *bit,
-        void *cl), void *cl);
+int bit2_put(T bit2, int bitnum, int row, int col);
+int bit2_get(T bit2, int row, int col);
+int bit2_width(T bit2); 
+int bit2_height(T bit2);
+void bit2_free(T * bit2);
+void bit2_map_row_major(T bit2, void apply(int row, int col,
+        T bitarr, int bitval, void *cl), void *cl);
+void bit2_map_col_major(T bit2, void apply(int row, int col,
+        T bitarr, int bitval, void *cl), void *cl);
 
 #undef T
 #endif
--- uarray2.h
+++ uarray2.h
@@ -4,22 +4,21 @@
  * uarray2.h
  */
 
-#ifdef USEARRAY2_H_
-#define USEARRAY2_H_
+#ifdef UARRAY2_H_
+#define UARRAY2_H_
 
-#define T UArray2_T
+#define T uarray2_T
 typedef struct T *T;
  
-T * UArray_new(int height, int width, int dataSize);
-void * UArray2_at(T UArray2, int row, int col);
-int UArray2_width(T UArray2); 
-int UArray2_height(T UArray2);
-int UArray2_height(T UArray2);
-void UArray2_free(T * UArray2);
-void UArray2_map_row_major(T UArray2, void apply(void *value,
-        void *cl), void *cl);
-void UArray2_map_col_major(T UArray2, void apply(void *value,
-        void *cl), void *cl);
+T * uarray2_new(int height, int width, int dataSize);
+void * uarray2_at(T uarray2, int row, int col);
+int uarray2_width(T uarray2); 
+int uarray2_height(T uarray2);
+void uarray2_free(T * uarray2);
+void uarray2_map_row_major(T uarray2, void apply(int row, int col,
+        T array, void *cl), void *cl);
+void uarray2_map_col_major(T uarray2, void apply(int row, int col,
+        T array, void *cl), void *cl);
 
 #undef T
 #endif
